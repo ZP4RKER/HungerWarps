d9fcd948dddc60b9c09713761899d77aa0f146a4
ZP4RKER <ZP4RKER@PROGRAMMER.NET>
2016-10-17 16:28:11 +1100

    Updated pom.xml

---
+++ src/main/java/com/zp4rker/hwarps/HungerWarps.java
@@ -0,0 +1,15 @@
+package com.zp4rker.hwarps;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HungerWarps extends JavaPlugin {
+	
+	public void onEnable() {
+		// onEnable code
+	}
+	
+	public void onDisable() {
+		// onDisable code
+	}
+
+}
